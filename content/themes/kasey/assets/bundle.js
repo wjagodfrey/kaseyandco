362bd3f65053e44c7bfeace5c82e0eff7755cd76
Wilfred Godfrey <w.j.godfrey@gmail.com>
2016-03-26 15:29:22 +1300

    Add sticky footer, and basic site layout.

--- content/themes/kasey/assets/bundle.js
+++ content/themes/kasey/assets/bundle.js
@@ -57,10 +57,14 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var map = {
-		"./config/config.scss": 2,
-		"./content/content.scss": 6,
-		"./css-reset/css-reset.scss": 8,
-		"./page-content/page-content.scss": 10
+		"./base/base.scss": 2,
+		"./config/config.scss": 6,
+		"./content/content.scss": 8,
+		"./css-reset/css-reset.scss": 10,
+		"./footer/footer.scss": 18,
+		"./header/header.scss": 12,
+		"./navigation/navigation.scss": 14,
+		"./page-content/page-content.scss": 16
 	};
 	function webpackContext(req) {
 		return __webpack_require__(webpackContextResolve(req));
@@ -92,8 +96,8 @@
 	if(false) {
 		// When the styles change, update the <style> tags
 		if(!content.locals) {
-			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./config.scss", function() {
-				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./config.scss");
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./base.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./base.scss");
 				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 				update(newContent);
 			});
@@ -111,7 +115,7 @@
 
 
 	// module
-	exports.push([module.id, "", ""]);
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\nhtml body {\n  height: 100vh;\n  font-family: Heiti SC; }\n  html body h1 {\n    font-size: 2.5rem; }\n    @media screen and (min-width: 600px) {\n      html body h1 {\n        font-size: 3.5rem; } }\n    @media screen and (min-width: 800px) {\n      html body h1 {\n        font-size: 4.5rem; } }\n  html body h2 {\n    font-size: 2rem; }\n  html body h3 {\n    font-size: 2rem; }\n  html body h4 {\n    font-size: 1.5rem; }\n  html body h5 {\n    font-size: 1rem; }\n  html body h6 {\n    font-size: 1rem; }\n", ""]);
 
 	// exports
 
@@ -442,8 +446,8 @@
 	if(false) {
 		// When the styles change, update the <style> tags
 		if(!content.locals) {
-			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./content.scss", function() {
-				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./content.scss");
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./config.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./config.scss");
 				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 				update(newContent);
 			});
@@ -461,7 +465,7 @@
 
 
 	// module
-	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.content {\n  max-width: 1200px;\n  margin-left: auto;\n  margin-right: auto; }\n  .content::after {\n    clear: both;\n    content: \"\";\n    display: table; }\n  .content .page-content {\n    float: left;\n    display: block;\n    margin-right: 2.35765%;\n    width: 100%;\n    margin-left: 0%; }\n    .content .page-content:last-child {\n      margin-right: 0; }\n    @media screen and (min-width: 720px) {\n      .content .page-content {\n        float: left;\n        display: block;\n        margin-right: 2.35765%;\n        width: 65.88078%;\n        margin-left: 17.05961%; }\n        .content .page-content:last-child {\n          margin-right: 0; } }\n", ""]);
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n", ""]);
 
 	// exports
 
@@ -482,8 +486,8 @@
 	if(false) {
 		// When the styles change, update the <style> tags
 		if(!content.locals) {
-			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./css-reset.scss", function() {
-				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./css-reset.scss");
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./content.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./content.scss");
 				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 				update(newContent);
 			});
@@ -501,7 +505,7 @@
 
 
 	// module
-	exports.push([module.id, "/* http://meyerweb.com/eric/tools/css/reset/ \n   v2.0 | 20110126\n   License: none (public domain)\n*/\nhtml, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {\n  margin: 0;\n  padding: 0;\n  border: 0;\n  font-size: 100%;\n  font: inherit;\n  vertical-align: baseline; }\n\n/* HTML5 display-role reset for older browsers */\narticle, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {\n  display: block; }\n\nbody {\n  line-height: 1; }\n\nol, ul {\n  list-style: none; }\n\nblockquote, q {\n  quotes: none; }\n\nblockquote:before, blockquote:after {\n  content: '';\n  content: none; }\n\nq:before, q:after {\n  content: '';\n  content: none; }\n\ntable {\n  border-collapse: collapse;\n  border-spacing: 0; }\n", ""]);
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.content {\n  max-width: 1200px;\n  margin-left: auto;\n  margin-right: auto;\n  min-height: 100%;\n  margin-bottom: -40px; }\n  .content::after {\n    clear: both;\n    content: \"\";\n    display: table; }\n  .content:after {\n    content: \"\";\n    display: block;\n    height: 40px; }\n  .content .content__main {\n    float: left;\n    display: block;\n    margin-right: 2.35765%;\n    width: 82.94039%;\n    margin-left: 8.5298%; }\n    .content .content__main:last-child {\n      margin-right: 0; }\n    @media screen and (min-width: 600px) {\n      .content .content__main {\n        float: left;\n        display: block;\n        margin-right: 2.35765%;\n        width: 65.88078%;\n        margin-left: 17.05961%; }\n        .content .content__main:last-child {\n          margin-right: 0; } }\n    .content .content__main h1, .content .content__main h2, .content .content__main h3, .content .content__main h4, .content .content__main h5, .content .content__main h6 {\n      padding: 1rem 0 1.5rem 0; }\n\n.home-page .content__main {\n  padding: 4rem 0; }\n", ""]);
 
 	// exports
 
@@ -522,8 +526,8 @@
 	if(false) {
 		// When the styles change, update the <style> tags
 		if(!content.locals) {
-			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./page-content.scss", function() {
-				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./page-content.scss");
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./css-reset.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./css-reset.scss");
 				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 				update(newContent);
 			});
@@ -541,7 +545,167 @@
 
 
 	// module
-	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.content {\n  max-width: 1200px;\n  margin-left: auto;\n  margin-right: auto; }\n  .content::after {\n    clear: both;\n    content: \"\";\n    display: table; }\n  .content .page-content {\n    float: left;\n    display: block;\n    margin-right: 2.35765%;\n    width: 100%;\n    margin-left: 0%; }\n    .content .page-content:last-child {\n      margin-right: 0; }\n    @media screen and (min-width: 720px) {\n      .content .page-content {\n        float: left;\n        display: block;\n        margin-right: 2.35765%;\n        width: 65.88078%;\n        margin-left: 17.05961%; }\n        .content .page-content:last-child {\n          margin-right: 0; } }\n", ""]);
+	exports.push([module.id, "/* http://meyerweb.com/eric/tools/css/reset/ \n   v2.0 | 20110126\n   License: none (public domain)\n*/\nhtml, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {\n  margin: 0;\n  padding: 0;\n  border: 0;\n  font-size: 100%;\n  font: inherit;\n  vertical-align: baseline; }\n\n/* HTML5 display-role reset for older browsers */\narticle, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {\n  display: block; }\n\nbody {\n  line-height: 1; }\n\nol, ul {\n  list-style: none; }\n\nblockquote, q {\n  quotes: none; }\n\nblockquote:before, blockquote:after {\n  content: '';\n  content: none; }\n\nq:before, q:after {\n  content: '';\n  content: none; }\n\ntable {\n  border-collapse: collapse;\n  border-spacing: 0; }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 12 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(13);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(5)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./header.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./header.scss");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(4)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\nheader.header {\n  position: relative; }\n  header.header .header__background {\n    height: 50vw;\n    background-position: center;\n    background-size: cover; }\n  header.header .header__content {\n    height: 50vw;\n    text-align: center; }\n    header.header .header__content .header__logo {\n      position: relative;\n      top: 20%; }\n      header.header .header__content .header__logo img {\n        -webkit-filter: brightness(1000%);\n        filter: brightness(1000%);\n        height: 25vw; }\n    header.header .header__content .header__text {\n      padding-top: 20%;\n      color: #ffffff; }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(15);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(5)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./navigation.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./navigation.scss");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 15 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(4)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.navigation {\n  display: none; }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 16 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(17);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(5)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./page-content.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./page-content.scss");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 17 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(4)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.content {\n  max-width: 1200px;\n  margin-left: auto;\n  margin-right: auto; }\n  .content::after {\n    clear: both;\n    content: \"\";\n    display: table; }\n  .content .page-content {\n    float: left;\n    display: block;\n    margin-right: 2.35765%;\n    width: 100%;\n    margin-left: 0%; }\n    .content .page-content:last-child {\n      margin-right: 0; }\n    @media screen and (min-width: 600px) {\n      .content .page-content {\n        float: left;\n        display: block;\n        margin-right: 2.35765%;\n        width: 65.88078%;\n        margin-left: 17.05961%; }\n        .content .page-content:last-child {\n          margin-right: 0; } }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 18 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(19);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(5)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./footer.scss", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/sass-loader/index.js!./footer.scss");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 19 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(4)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "html {\n  box-sizing: border-box; }\n\n*, *::after, *::before {\n  box-sizing: inherit; }\n\n.footer {\n  height: 40px; }\n", ""]);
 
 	// exports
 
