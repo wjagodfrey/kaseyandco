5a539c9fd1b24ccd97dffde4ff550531c04e1a53
Wilfred Godfrey <w.j.godfrey@gmail.com>
2016-03-27 19:35:14 +1300

    Add initial post styles.

--- content/themes/kasey/assets/bundle.js
+++ content/themes/kasey/assets/bundle.js
@@ -68,7 +68,8 @@
 		"./header/header.scss": 185,
 		"./navigation/navigation.scss": 187,
 		"./page-content/page-content.scss": 189,
-		"./pagination/pagination.scss": 191
+		"./pagination/pagination.scss": 191,
+		"./post/post.scss": 193
 	};
 	function webpackContext(req) {
 		return __webpack_require__(webpackContextResolve(req));
@@ -313,7 +314,7 @@
 /* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var __WEBPACK_AMD_DEFINE_RESULT__;var require;/* WEBPACK VAR INJECTION */(function(process, global, module) {/*!
+	var require;var __WEBPACK_AMD_DEFINE_RESULT__;/* WEBPACK VAR INJECTION */(function(process, global, module) {/*!
 	 * @overview es6-promise - a tiny implementation of Promises/A+.
 	 * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
 	 * @license   Licensed under MIT license
@@ -21722,6 +21723,13 @@
 /***/ },
 /* 190 */,
 /* 191 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 192 */,
+/* 193 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
